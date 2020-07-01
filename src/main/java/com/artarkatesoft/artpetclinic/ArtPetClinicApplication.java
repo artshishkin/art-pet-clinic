cc43f7da62ccda28c605fe29e48db4a0f93f3b47
admin <d.art.shishkin@gmail.com>
2020-07-01 23:15:40 +0300

    init

---
+++ src/main/java/com/artarkatesoft/artpetclinic/ArtPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.artarkatesoft.artpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/artarkatesoft/artpetclinic/ArtPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artarkatesoft.artpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
