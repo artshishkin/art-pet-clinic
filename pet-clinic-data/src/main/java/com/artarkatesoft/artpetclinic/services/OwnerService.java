2e693bb0f09232c5cced41127c388b296a4ec30f
admin <d.art.shishkin@gmail.com>
2020-07-02 21:48:59 +0300

    art-pet-clinic-10 Refactor Service interface to Common Base Interface (closes #10)

--- pet-clinic-data/src/main/java/com/artarkatesoft/artpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/artarkatesoft/artpetclinic/services/OwnerService.java
@@ -2,14 +2,6 @@ package com.artarkatesoft.artpetclinic.services;
 
 import com.artarkatesoft.artpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/com/artarkatesoft/artpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/artarkatesoft/artpetclinic/services/VetService.java
@@ -2,13 +2,5 @@ package com.artarkatesoft.artpetclinic.services;
 
 import com.artarkatesoft.artpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet owner);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
