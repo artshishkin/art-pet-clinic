6d404e9382faed42c398a685a5d0b242f7bbad50
admin <d.art.shishkin@gmail.com>
2020-07-03 13:23:49 +0300

    art-pet-clinic-13 Create Owner index page and controller (added Controller) (closes #13)

---
+++ pet-clinic-web/src/main/java/com/artarkatesoft/artpetclinic/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package com.artarkatesoft.artpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("owners")
+public class OwnerController {
+
+    @RequestMapping({"","index","index.html"})
+    public String list(){
+        return "owners/index";
+    }
+
+}
