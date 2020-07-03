3fe6e58699d4dcb410531eb9014c3462a222a41a
Art <d.art.shishkin@gmail.com>
2020-07-03 17:54:43 +0300

    art-pet-clinic-15 List all vets on Vet index page (Controller too)

--- pet-clinic-web/src/main/java/com/artarkatesoft/artpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/artarkatesoft/artpetclinic/controllers/VetController.java
@@ -1,13 +1,33 @@
 package com.artarkatesoft.artpetclinic.controllers;
 
+import com.artarkatesoft.artpetclinic.model.BaseEntity;
+import com.artarkatesoft.artpetclinic.model.Vet;
+import com.artarkatesoft.artpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping("vets")
-    public String list(){
+    public String list(Model model) {
+
+        List<Vet> vetList = vetService.findAll()
+                .stream()
+                .sorted(Comparator.comparing(BaseEntity::getId))
+                .collect(Collectors.toList());
+        model.addAttribute("vets", vetList);
         return "vets/index";
     }
 
