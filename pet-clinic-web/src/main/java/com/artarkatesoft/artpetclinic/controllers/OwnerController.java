b45a34ef2cf07d28c09508376b233242ad845091
Art <d.art.shishkin@gmail.com>
2020-07-03 17:43:03 +0300

    82 art-pet-clinic-16 List all Owners on Owners Index Page (closes #16)

--- pet-clinic-web/src/main/java/com/artarkatesoft/artpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/artarkatesoft/artpetclinic/controllers/OwnerController.java
@@ -1,14 +1,27 @@
 package com.artarkatesoft.artpetclinic.controllers;
 
+import com.artarkatesoft.artpetclinic.model.Owner;
+import com.artarkatesoft.artpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @Controller
 @RequestMapping("owners")
 public class OwnerController {
 
-    @RequestMapping({"","index","index.html"})
-    public String list(){
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"", "index", "index.html"})
+    public String list(Model model) {
+        Set<Owner> owners = ownerService.findAll();
+        model.addAttribute("owners", owners);
         return "owners/index";
     }
 
