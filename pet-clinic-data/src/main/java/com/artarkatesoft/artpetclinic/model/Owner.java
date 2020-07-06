fbda04fc65e809b6610edaccf6b85e20bb7034b9
admin <d.art.shishkin@gmail.com>
2020-07-06 15:20:25 +0300

    118 art-pet-clinic-26 Create PetType, Pet and Visit Entities (closes #26)

--- pet-clinic-data/src/main/java/com/artarkatesoft/artpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/artarkatesoft/artpetclinic/model/Owner.java
@@ -1,4 +1,14 @@
 package com.artarkatesoft.artpetclinic.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
 public class Owner extends Person {
+    private Set<Pet> pets;
+
+
 }
