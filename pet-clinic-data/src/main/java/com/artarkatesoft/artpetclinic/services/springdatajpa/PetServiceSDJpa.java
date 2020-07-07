1065d6981188a7d63f29f724772bef3e0026ed42
admin <d.art.shishkin@gmail.com>
2020-07-08 00:01:53 +0300

    art-pet-clinic-43 Create Spring Data JPA Pets Service. close #43

---
+++ pet-clinic-data/src/main/java/com/artarkatesoft/artpetclinic/services/springdatajpa/PetServiceSDJpa.java
@@ -0,0 +1,46 @@
+package com.artarkatesoft.artpetclinic.services.springdatajpa;
+
+import com.artarkatesoft.artpetclinic.model.Pet;
+import com.artarkatesoft.artpetclinic.repositories.PetRepository;
+import com.artarkatesoft.artpetclinic.services.PetService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Component
+@RequiredArgsConstructor
+@Profile("springdatajpa")
+public class PetServiceSDJpa implements PetService {
+
+    private final PetRepository petRepository;
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return petRepository.save(pet);
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        return StreamSupport.stream(petRepository.findAll().spliterator(), false)
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        petRepository.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petRepository.deleteById(id);
+    }
+}
