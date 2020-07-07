27499c1e35929c17218090c71868af1400c8e91d
admin <d.art.shishkin@gmail.com>
2020-07-07 22:29:09 +0300

    145 art-pet-clinic-35 Implement Base Entity (JPA) (closes #35)

--- pet-clinic-data/src/main/java/com/artarkatesoft/artpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/artarkatesoft/artpetclinic/model/BaseEntity.java
@@ -3,11 +3,18 @@ package com.artarkatesoft.artpetclinic.model;
 
 import lombok.Data;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 @Data
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
