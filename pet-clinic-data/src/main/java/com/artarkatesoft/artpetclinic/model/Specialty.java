aba02f13f1628788103f6e00bec2be04b77a70a9
admin <d.art.shishkin@gmail.com>
2020-07-07 22:54:57 +0300

    147 art-pet-clinic-37 Convert Vets to JPA Entities (closes #37)

--- pet-clinic-data/src/main/java/com/artarkatesoft/artpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/com/artarkatesoft/artpetclinic/model/Specialty.java
@@ -3,8 +3,15 @@ package com.artarkatesoft.artpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 @Getter
 @Setter
-public class Specialty extends BaseEntity{
+public class Specialty extends BaseEntity {
+    @Column(name = "description")
     private String description;
 }
