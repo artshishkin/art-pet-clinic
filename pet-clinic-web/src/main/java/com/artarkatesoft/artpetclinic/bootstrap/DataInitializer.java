fa7481b53754f34f0c4bb4925c7bc7a3e79208d4
admin <d.art.shishkin@gmail.com>
2020-07-09 15:32:35 +0300

    wrapping method `run` in DataInitializer into @Transactional to prevent persisting errors

--- pet-clinic-web/src/main/java/com/artarkatesoft/artpetclinic/bootstrap/DataInitializer.java
+++ pet-clinic-web/src/main/java/com/artarkatesoft/artpetclinic/bootstrap/DataInitializer.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 
 @Component
@@ -19,6 +20,7 @@ public class DataInitializer implements CommandLineRunner {
     private final SpecialtyService specialtyService;
 
     @Override
+    @Transactional
     public void run(String... args) throws Exception {
 
         boolean dataLoaded = !ownerService.findAll().isEmpty();
