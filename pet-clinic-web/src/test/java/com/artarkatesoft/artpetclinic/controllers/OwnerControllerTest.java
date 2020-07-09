319550286e33a946cc40d2eb7f1d6bc68a8ebfb9
Art <d.art.shishkin@gmail.com>
2020-07-09 21:46:56 +0300

    186 art-pet-clinic-53 Write Spring MVC Tests for Owner Controller (@WebMvcTest(OwnerController.class)) (closes #53)

---
+++ pet-clinic-web/src/test/java/com/artarkatesoft/artpetclinic/controllers/OwnerControllerTest.java
@@ -0,0 +1,70 @@
+package com.artarkatesoft.artpetclinic.controllers;
+
+import com.artarkatesoft.artpetclinic.model.Owner;
+import com.artarkatesoft.artpetclinic.services.OwnerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Collections;
+
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.ResultMatcher.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(OwnerController.class)
+class OwnerControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+    //WebMvcTest - 1063ms
+
+    @MockBean
+    OwnerService ownerService;
+
+    private Owner owner;
+
+    @BeforeEach
+    void setUp() {
+        owner = Owner.builder()
+                .firstName("Art")
+                .lastName("Shyshkin")
+                .city("City1")
+                .address("Address1")
+                .telephone("123")
+                .build();
+        owner.setId(1L);
+    }
+
+    @Test
+    void list() throws Exception {
+        given(ownerService.findAll()).willReturn(Collections.singleton(owner));
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/index"))
+                .andExpect(model().attributeExists("owners"))
+                .andExpect(model().attribute("owners", hasSize(1)));
+        then(ownerService).should().findAll();
+    }
+
+    @Test
+    void find() throws Exception {
+        mockMvc.perform(get("/owners/find"))
+                .andExpect(matchAll(
+                        status().isOk(),
+                        view().name("not_implemented")
+                ));
+        then(ownerService).shouldHaveNoInteractions();
+    }
+}
