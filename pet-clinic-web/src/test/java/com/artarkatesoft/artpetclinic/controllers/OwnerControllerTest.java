723bd92b602c3d32b776db552d534ddfb3a58186
admin <d.art.shishkin@gmail.com>
2020-07-15 12:42:15 +0300

    212 Display Owner Record (HTML). closes #54

--- pet-clinic-web/src/test/java/com/artarkatesoft/artpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/artarkatesoft/artpetclinic/controllers/OwnerControllerTest.java
@@ -2,30 +2,27 @@ package com.artarkatesoft.artpetclinic.controllers;
 
 import com.artarkatesoft.artpetclinic.model.Owner;
 import com.artarkatesoft.artpetclinic.services.OwnerService;
+import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.Collections;
 
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.ResultMatcher.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.ResultMatcher.matchAll;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -41,25 +38,25 @@ class OwnerControllerTest {
     @InjectMocks
     OwnerController ownerController;
 
-    private Owner owner;
+    private Owner defaultOwner;
 
     @BeforeEach
     void setUp() {
-        owner = Owner.builder()
+        defaultOwner = Owner.builder()
                 .firstName("Art")
                 .lastName("Shyshkin")
                 .city("City1")
                 .address("Address1")
                 .telephone("123")
                 .build();
-        owner.setId(1L);
+        defaultOwner.setId(1L);
 
         mockMvc = MockMvcBuilders.standaloneSetup(ownerController).build();
     }
 
     @Test
     void list() throws Exception {
-        given(ownerService.findAll()).willReturn(Collections.singleton(owner));
+        given(ownerService.findAll()).willReturn(Collections.singleton(defaultOwner));
         mockMvc.perform(get("/owners"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("owners/index"))
@@ -76,5 +73,23 @@ class OwnerControllerTest {
                         view().name("not_implemented")
                 ));
         then(ownerService).shouldHaveNoInteractions();
+    }
+
+    @Test
+    void testShowOwnerDetails() throws Exception {
+        //given
+        Long ownerId = defaultOwner.getId();
+        given(ownerService.findById(anyLong())).willReturn(defaultOwner);
+
+        //when
+        mockMvc.perform(get("/owners/{ownerId}", ownerId))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attributeExists("owner"))
+                .andExpect(model().attribute("owner", CoreMatchers.notNullValue()))
+                .andExpect(model().attribute("owner", hasProperty("id", is(ownerId))))
+        ;
+        //then
+        then(ownerService).should().findById(eq(ownerId));
     }
 }
