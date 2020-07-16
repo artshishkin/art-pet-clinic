78ff84f54eb3206c245361c4cde81ff7d98e7843
admin <d.art.shishkin@gmail.com>
2020-07-16 17:02:14 +0300

    218 Model Attribute #57

---
+++ pet-clinic-web/src/main/java/com/artarkatesoft/artpetclinic/controllers/PetController.java
@@ -0,0 +1,40 @@
+package com.artarkatesoft.artpetclinic.controllers;
+
+import com.artarkatesoft.artpetclinic.model.Owner;
+import com.artarkatesoft.artpetclinic.model.PetType;
+import com.artarkatesoft.artpetclinic.services.OwnerService;
+import com.artarkatesoft.artpetclinic.services.PetTypeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+
+@Controller
+@RequestMapping("owners/{ownerId}")
+@RequiredArgsConstructor
+public class PetController {
+
+    private final PetTypeService petTypeService;
+    private final OwnerService ownerService;
+
+    @ModelAttribute("types")
+    public Collection<PetType> populatePetTypes() {
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable("ownerId") Long ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+
+}
